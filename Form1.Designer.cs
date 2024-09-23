f93b1fc668757753dbbae3a28da2071f3fe83499
ParagNirwan <pnirwan9@gmail.com>
2024-09-23 23:35:12 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ColoredFolders
+{
+    partial class ColoredFolders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ColoredFolders));
+            this.On = new System.Windows.Forms.RadioButton();
+            this.Off = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // On
+            // 
+            this.On.AutoSize = true;
+            this.On.Location = new System.Drawing.Point(35, 52);
+            this.On.Name = "On";
+            this.On.Size = new System.Drawing.Size(64, 29);
+            this.On.TabIndex = 0;
+            this.On.Text = "On";
+            this.On.UseVisualStyleBackColor = true;
+            this.On.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Off
+            // 
+            this.Off.AutoSize = true;
+            this.Off.Checked = true;
+            this.Off.Location = new System.Drawing.Point(35, 87);
+            this.Off.Name = "Off";
+            this.Off.Size = new System.Drawing.Size(63, 29);
+            this.Off.TabIndex = 1;
+            this.Off.TabStop = true;
+            this.Off.Text = "Off";
+            this.Off.UseVisualStyleBackColor = true;
+            this.Off.CheckedChanged += new System.EventHandler(this.Off_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Made By Parag Nirwan";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(391, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contex Menu Option (Right Click on Folder)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ColoredFolders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 192);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Off);
+            this.Controls.Add(this.On);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ColoredFolders";
+            this.Text = "Colored Folders";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton On;
+        private System.Windows.Forms.RadioButton Off;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Win32;  // Import for working with the Windows Registry
+
+namespace ColoredFolders
+{
+    public partial class ColoredFolders : Form
+    {
+        private const string registryPath = @"Software\ColoredFolders";  // The location to save the On/Off setting
+        private const string folderRegistryPath = @"Folder\shell\coloredFolders";  // Path for folder color settings in the registry
+
+        public ColoredFolders()
+        {
+            InitializeComponent();
+            LoadSettings();  // Load the saved On/Off state when the form loads
+        }
+
+        // Event handler when the 'On' RadioButton is selected
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+
+            if (radioButton != null && radioButton.Checked)
+            {
+                // This will be executed when the 'On' RadioButton is selected
+                WriteOnOffRegistry("On");  // Save the On state
+                WriteFolderColorRegistry();  // Write the folder color settings to the registry
+                MessageBox.Show("Registry updated and Colored Folders is On.");
+            }
+        }
+
+        // Event handler when the 'Off' RadioButton is selected
+        private void Off_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+
+            if (radioButton != null && radioButton.Checked)
+            {
+                // This will be executed when the 'Off' RadioButton is selected
+                WriteOnOffRegistry("Off");  // Save the Off state
+                RemoveFolderColorRegistry();  // Remove folder color settings from the registry
+                MessageBox.Show("Colored Folders feature disabled.");
+            }
+        }
+
+        // Function to save the On/Off setting to the registry
+        private void WriteOnOffRegistry(string value)
+        {
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.CreateSubKey(registryPath);
+                if (key != null)
+                {
+                    key.SetValue("FolderColorFeature", value, RegistryValueKind.String);  // Save "On" or "Off"
+                    key.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving the setting: " + ex.Message);
+            }
+        }
+
+        // Function to write the folder color settings to the registry (when "On" is selected)
+        private void WriteFolderColorRegistry()
+        {
+            try
+            {
+                RegistryKey key = Registry.ClassesRoot.CreateSubKey(folderRegistryPath);
+                if (key != null)
+                {
+                    key.SetValue("MUIVerb", "Set Folder Color", RegistryValueKind.String);
+                    key.SetValue("SubCommands", "Red; Green; Blue; Purple; Pink; Orange; Teal; Grey;", RegistryValueKind.String);
+                    key.SetValue("Icon", @"C:\Users\pnirw\Documents\ColoredFolders\assets\Blue (2).ico", RegistryValueKind.String);
+                    key.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error writing to registry: " + ex.Message);
+            }
+        }
+
+        // Function to remove the folder color settings from the registry (when "Off" is selected)
+        private void RemoveFolderColorRegistry()
+        {
+            try
+            {
+                RegistryKey key = Registry.ClassesRoot.OpenSubKey(folderRegistryPath, true);
+                if (key != null)
+                {
+                    key.DeleteValue("MUIVerb", false);  // Optionally remove values if needed
+                    key.DeleteValue("SubCommands", false);
+                    key.DeleteValue("Icon", false);
+                    key.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error modifying the registry: " + ex.Message);
+            }
+        }
+
+        // Function to load the On/Off setting from the registry
+        private void LoadSettings()
+        {
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(registryPath);
+                if (key != null)
+                {
+                    string featureState = key.GetValue("FolderColorFeature", "Off").ToString();  // Default to "Off" if not found
+                    key.Close();
+
+                    // Set the radio buttons based on the saved value
+                    if (featureState == "On")
+                    {
+                        On.Checked = true;  // On is selected
+                    }
+                    else
+                    {
+                        Off.Checked = true;  // Off is selected
+                    }
+                }
+                else
+                {
+                    // If the key does not exist, default to Off (first run)
+                    Off.Checked = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading the setting: " + ex.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
